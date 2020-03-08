84446311dab39e005af45d8e539529a3f6618169
Frantisek Rapcak <frantisek.rapcak@direct.cz>
2020-03-08 13:16:08 +0100

    JS: ShrinkHeader onScroll

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-console.log('app.js works');
+console.info('✅ app.js works');
 
-window.onload = function() {
-  console.log('window.onload works');
+
+
+//
+//  DOM ready
+//
+document.addEventListener("DOMContentLoaded", function(){
+  console.info('✅ DOMContentLoaded');
+});
+
+
+
+
+//
+//  When the user scrolls down 80px from the top of the document,
+//  resize the navbar's padding and the logo's font size
+//
+window.onscroll = function() {
+  console.debug("scrollTop: " + document.documentElement.scrollTop);
+  shrinkHeader();
 };
+
+
+
+
+//
+//  Shrink Header onScroll
+//
+function shrinkHeader() {
+  var scrollTop = 50;
+
+  if ( document.body.scrollTop > scrollTop || document.documentElement.scrollTop > scrollTop ) {
+    document.body.classList.add("js__shrink-navigation");
+  }
+  else {
+    document.body.classList.remove("js__shrink-navigation");
+  }
+}
